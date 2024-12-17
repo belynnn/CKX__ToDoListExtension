6286aac45726a06331c9fed56b225ecfe25b6cf2
Deborah Clerckx <d.clerckx@interface3.be>
2024-12-17 15:02:43 +0100

    init

---
+++ script.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var taskArr = [];
+
+    const updateView = () => {
+        const dTasksList = document.getElementById("d_tasks_list");
+
+        var child = dTasksList.lastChild;
+        while(child) {
+            dTasksList.removeChild(child);
+            child = dTasksList.lastChild;
+        }
+
+        taskArr.forEach((Element, index) => {
+            const newTask = document.createElement("div");
+            newTask.setAttribute("class", "task-div");
+
+            const taskText = document.createElement("div");
+            taskText.setAttribute("class", Element.isDone ? "task-text task-completed" : "task-text");
+            taskText.innerHTML = (index + 1) + ". " + Element.task;
+
+            const taskControls = document.createElement("div");
+            taskControls.setAttribute("class", "task-controls");
+
+            const taskEdit = document.createElement("button");
+            taskEdit.innerHTML = "Edit";
+            taskEdit.setAttribute("id", index + "edit");
+            taskEdit.setAttribute("class", "btn-task-edit btn");
+            taskEdit.addEventListener("click", (event) => editTask(event.target.id));
+
+            const taskDelete = document.createElement("button");
+            taskDelete.innerHTML = "Delete";
+            taskDelete.setAttribute("id", index + "delete");
+            taskDelete.setAttribute("class", "btn-task-delete btn");
+            taskDelete.addEventListener("click", (event) => deleteTask(event.target.id));
+
+            const taskDo = document.createElement("button");
+            taskDo.innerHTML = Element.isDone ? "Undo" : "Done";
+            taskDo.setAttribute("id", index + "do");
+            taskDo.setAttribute("class", "btn-task-do btn");
+            taskDo.addEventListener("click", (event) => doTask(event.target.id));
+
+            taskControls.appendChild(taskEdit);
+            taskControls.appendChild(taskDelete);
+            taskControls.appendChild(taskDo);
+
+            newTask.appendChild(taskText);
+            newTask.appendChild(taskControls);
+
+            d_tasks_list.appendChild(newTask);
+        });
+    }
+
+    const addTask = (isDone) => {
+        const task = document.getElementById("ipt_task").value;
+        if(task === null || task.trim() === "") return;
+        taskArr.push({task, isDone});
+        localStorage.setItem("savedTasks", JSON.stringify(taskArr));
+        updateView();
+
+        const taskInput = document.getElementById("ipt_task");
+        taskInput.value = "";
+    }
+
+    const editTask = (id) => {
+        const taskIndex = parseInt(id[0]);
+        const taskText = taskArr[taskIndex].task;
+        taskArr.splice(taskIndex, 1);
+        localStorage.setItem("savedTasks", JSON.stringify(taskArr));
+        updateView();
+
+        const taskInput = document.getElementById("ipt_task");
+        taskInput.value = taskText;
+    }
+
+    const deleteTask = (id) => {
+        const taskIndex = parseInt(id[0]);
+        taskArr.splice(taskIndex, 1);
+        localStorage.setItem("savedTasks", JSON.stringify(taskArr));
+        updateView();
+    }
+
+    const doTask = (id) => {
+        const taskIndex = parseInt(id[0]);
+        taskArr[taskIndex].isDone = !taskArr[taskIndex].isDone;
+        localStorage.setItem("savedTasks", JSON.stringify(taskArr));
+        updateView();
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+        if(savedTasks !== null) taskArr = [...savedTasks];
+        updateView();
+    })
+
+    document.getElementById("btn_submit_task").addEventListener("click", () => addTask(false));
+
+    document.getElementById("btn_clear_task").addEventListener("click", () => {
+        localStorage.clear();
+        taskArr = [];
+        updateView();
+    })
+});
